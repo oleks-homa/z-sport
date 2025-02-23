65b9c1bbc7f32e3010175bde842c9afc8518437b
Oleksii Khomaziuk <oleks.homa@gmail.com>
2025-02-23 12:39:46 +0100

    changed gallery

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -61,89 +61,55 @@ const Home = () => {
 
 	const gallery = [
 		{
-			src: '/images/gallery1.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery1.jpg'
 		},
 		{
-			src: '/images/gallery2.jpg',
-			width: 500,
-			height: 400
+			src: '/images/gallery2.jpg'
 		},
 		{
-			src: '/images/gallery3.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery3.jpg'
 		},
 		{
-			src: '/images/gallery4.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery4.jpg'
 		},
 		{
-			src: '/images/gallery5.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery5.jpg'
 		},
 		{
-			src: '/images/gallery6.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery6.jpg'
 		},
 		{
-			src: '/images/gallery7.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery7.jpg'
 		},
 		{
-			src: '/images/gallery8.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery8.jpg'
 		},
 		{
-			src: '/images/gallery9.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery9.jpg'
 		},
 		{
-			src: '/images/gallery10.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery10.jpg'
 		},
 		{
-			src: '/images/gallery11.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery11.jpg'
 		},
 		{
-			src: '/images/gallery12.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery12.jpg'
 		},
 		{
-			src: '/images/gallery13.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery13.jpg'
 		},
 		{
-			src: '/images/gallery14.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery14.jpg'
 		},
 		{
-			src: '/images/gallery15.jpg',
-			width: 500,
-			height: 300
+			src: '/images/gallery15.jpg'
 		},
 		{
-			src: '/images/gallery16.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery16.jpg'
 		},
 		{
-			src: '/images/gallery17.jpg',
-			width: 400,
-			height: 300
+			src: '/images/gallery17.jpg'
 		},
 	]
 
@@ -197,11 +163,28 @@ const Home = () => {
 					</button>
 				</div>
 			</div>
-			<div className='block mb-5 mx-5'>
-				<RowsPhotoAlbum
-					photos={gallery}
-					onClick={({ index }) => setBoxIndex(index)}
-				/>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-5 mx-5 items-center'>
+				{/* {gallery.map((image, index) => (
+					<img src={image.src} alt="Image" onClick={() => setBoxIndex(index)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				))} */}
+				<img src='/images/gallery1.jpg' alt="Image" onClick={() => setBoxIndex(0)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery2.jpg' alt="Image" onClick={() => setBoxIndex(1)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery3.jpg' alt="Image" onClick={() => setBoxIndex(2)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery6.jpg' alt="Image" onClick={() => setBoxIndex(5)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery9.jpg' alt="Image" onClick={() => setBoxIndex(8)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery8.jpg' alt="Image" onClick={() => setBoxIndex(7)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery5.jpg' alt="Image" onClick={() => setBoxIndex(4)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery10.jpg' alt="Image" onClick={() => setBoxIndex(9)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery11.jpg' alt="Image" onClick={() => setBoxIndex(10)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery12.jpg' alt="Image" onClick={() => setBoxIndex(11)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery13.jpg' alt="Image" onClick={() => setBoxIndex(12)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery14.jpg' alt="Image" onClick={() => setBoxIndex(13)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery15.jpg' alt="Image" onClick={() => setBoxIndex(14)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery16.jpg' alt="Image" onClick={() => setBoxIndex(15)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery17.jpg' alt="Image" onClick={() => setBoxIndex(16)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery4.jpg' alt="Image" onClick={() => setBoxIndex(3)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+				<img src='/images/gallery7.jpg' alt="Image" onClick={() => setBoxIndex(6)} className='w-full h-fit object-cover rounded-lg cursor-pointer' />
+
 				<Lightbox
 					slides={gallery}
 					open={boxIndex >= 0}
